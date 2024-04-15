c7f625556f9e555078d0bfa663c2c157166f57f9
Yogesh Poul <yogeshpoul9999@gmail.com>
2024-04-15 20:35:38 +0530

    landing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,8 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route path="/signup" element={<Signup/>}/>
-            <Route path="/signin" element={<Signin/>}/>
+            {/* <Route path="/signin" element={<Signin/>}/> */}
+            <Route path="/" element={<Signin/>}/>
             <Route path="/dashboard" element={<Dashboard/>}/>
             <Route path="/send" element={<SendMoney/>}/>
             
