887bb62e6b02de3923baf3e67c49a22b1b440ac4
Yogesh Poul <yogeshpoul9999@gmail.com>
2024-04-24 21:05:41 +0530

    (1 + Math.random() * 10000).toFixed(2)

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -43,7 +43,8 @@ router.post("/signup", async (req, res) => {
 
     await Account.create({
         userId,
-        balance: 1 + Math.random() * 10000
+        // balance: 1 + Math.random() * 10000
+        balance: (1 + Math.random() * 10000).toFixed(2)
     })
 
     const token = jwt.sign({
